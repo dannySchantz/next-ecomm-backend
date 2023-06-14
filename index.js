cb525ad23d48f06190068d12f3a5e171c5ea1ed7
Danny Schantz <dschantz801@gmail.com>
2023-06-14 09:23:11 +0800

    second commit

--- index.js
+++ index.js
@@ -1,14 +1,89 @@
-import express from "express"
-import prisma from "./src/utils/prisma.js"
+import express from "express";
+import prisma from "./src/utils/prisma.js";
+import { Prisma } from "@prisma/client";
+import bcrypt from "bcryptjs";
+import cors from "cors";
 
-const app = express()
-const port = process.env.PORT || 8080
+const app = express();
+const port = process.env.PORT || 8080;
 
-app.get('/', async (req, res) => {
-  const allUsers = await prisma.user.findMany()
-  res.json(allUsers)
+app.use(express.json());
+app.use(cors());
+
+app.get('/products/:id', (req, res, next) => {
+  res.json({ msg: 'This is CORS-enabled for all origins!' });
+});
+
+app.get("/users", async (req, res) => {
+  const allUsers = await prisma.user.findMany();
+  res.json(allUsers);
+});
+
+function filter(obj, ...keys) {
+  return keys.reduce((a, c) => ({ ...a, [c]: obj[c]}), {})
+}
+
+
+
+function validateUser(input) {
+  const validationErrors = {}
+
+  if (!('name' in input) || input['name'].length == 0) {
+    validationErrors['name'] = 'cannot be blank'
+  }
+
+  if (!('email' in input) || input['email'].length == 0) {
+    validationErrors['email'] = 'cannot be blank'
+  }
+
+  if (!('password' in input) || input['password'].length == 0) {
+    validationErrors['password'] = 'cannot be blank'
+  }
+
+  if ('password' in input && input['password'].length < 8) {
+    validationErrors['password'] = 'should be at least 8 characters'
+  }
+
+  if ('email' in input && !input['email'].match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
+    validationErrors['email'] = 'is invalid'
+  }
+
+  return validationErrors
+}
+
+// some code
+
+app.post('/users', async (req, res) => {
+  const data = req.body
+
+
+  const validationErrors = validateUser(data)
+
+  if (Object.keys(validationErrors).length != 0) return res.status(400).send({
+    error: validationErrors
+  })
+
+  data.password = bcrypt.hashSync(data.password, 8);
+  prisma.user.create({
+    data
+  }).then(user => {
+    return res.json(filter(user, 'id', 'name', 'email'))
+
+  }).catch(err => {
+    // we have unique index on user's email field in our schema, Postgres throws an error when we try to create 2 users with the same email. here's how we catch the error and gracefully return a friendly message to the user.
+    if (err instanceof Prisma.PrismaClientKnownRequestError && err.code === 'P2002') {
+      const formattedError = {}
+      formattedError[`${err.meta.target[0]}`] = 'already taken'
+
+      return res.status(500).send({
+        error: formattedError
+      })
+    }
+    throw err
+  })
 })
+ 
 
 app.listen(port, () => {
-  console.log(`App started; listening on port ${port}`)
-})
+  console.log(`App started; listening on port ${port}`);
+});
