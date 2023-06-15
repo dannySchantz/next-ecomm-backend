95585d69a23221d8f0010dd9a199c9025e070a40
Danny Schantz <dschantz801@gmail.com>
2023-06-15 10:04:45 +0800

    finished testing

--- tests/endpoints/users.test.js
+++ tests/endpoints/users.test.js
@@ -12,52 +12,73 @@ async function cleanupDatabase() {
 }
 
 describe("POST /users", () => {
-  const user = {
-    name: 'John',
-    email: 'john9@example.com',
-    password: 'insecure',
-  }
+    const user = {
+        name: 'John',
+        email: 'john9@example.com',
+        password: 'insecure',
+    }
 
-  beforeAll(async () => {
-    await cleanupDatabase()
+    beforeAll(async () => {
+        await cleanupDatabase()
+    })
 
-  })
+    afterAll(async () => {
+        await cleanupDatabase()
+    })
 
-  afterAll(async () => {
-    await cleanupDatabase()
-  })
+    it("with valid data should return 200", async () => {
+        const response = await request(app)
+            .post("/users")
+            .send(user)
+            .set('Accept', 'application/json')
+        expect(response.statusCode).toBe(200);
+        expect(response.body.id).toBeTruthy;
+        expect(response.body.name).toBe(user.name);
+        expect(response.body.email).toBe(user.email);
+        expect(response.body.password).toBe(undefined);
+    });
 
-  it("with valid data should return 200", async () => {
-    const response = await request(app)
-      .post("/users")
-      .send(user)
-      .set('Accept', 'application/json')
-    expect(response.statusCode).toBe(200);
-    expect(response.body.id).toBeTruthy;
-    expect(response.body.name).toBe(user.name);
-    expect(response.body.email).toBe(user.email);
-    expect(response.body.password).toBe(undefined);
-  });
-})
+    it("with same email should fail", async () => {
+        const response = await request(app)
+            .post("/users")
+            .send(user)
+            .set('Accept', 'application/json')
+        expect(response.statusCode).toBe(500);
+        expect(response.body.error).toBeTruthy;
+        expect(response.body.error.email).toBe('already taken');
+    });
 
-it("with same email should fail", async () => {
-    const response = await request(app)
-      .post("/users")
-      .send(user)
-      .set('Accept', 'application/json')
-    expect(response.statusCode).toBe(500);
-    expect(response.body.error).toBeTruthy;
-    expect(response.body.error.email).toBe('already taken');
-  });
+    it("with invalid password should fail", async () => {
+        user.email = "unique@example.com"
+        user.password = "short"
+        const response = await request(app)
+            .post("/users")
+            .send(user)
+            .set('Accept', 'application/json')
+        expect(response.statusCode).toBe(400);
+        expect(response.body.error).toBeTruthy;
+        expect(response.body.error.password).toBe('should be at least 8 characters');
+    });
 
-it("with invalid password should fail", async () => {
-    user.email = "unique@example.com"
-    user.password = "short"
-    const response = await request(app)
-        .post("/users")
-        .send(user)
-        .set('Accept', 'application/json')
-    expect(response.statusCode).toBe(400);
-    expect(response.body.error).toBeTruthy;
-    expect(response.body.error.password).toBe('should be at least 8 characters');
+    it("with invalid email format should fail", async () => {
+        user.email = "invalid-email"
+        const response = await request(app)
+            .post("/users")
+            .send(user)
+            .set('Accept', 'application/json')
+        expect(response.statusCode).toBe(400);
+        expect(response.body.error).toBeTruthy;
+        expect(response.body.error.email).toBe('is invalid');
+    });
+
+    it("with blank name should fail", async () => {
+        user.name = ""
+        const response = await request(app)
+            .post("/users")
+            .send(user)
+            .set('Accept', 'application/json')
+        expect(response.statusCode).toBe(400);
+        expect(response.body.error).toBeTruthy;
+        expect(response.body.error.name).toBe('cannot be blank');
+    });  
 });
